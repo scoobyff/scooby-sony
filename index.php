01b3315cd3a9496bc3d46736ed5fcd632eff114b
scoobyff <pranishwarnetha02@gmail.com>
2025-05-24 00:50:58 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+// Set CORS headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Headers: *");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit();
+}
+
+$get = $_GET['get'];
+$mpdUrl = 'https://sonydaimenew.akamaized.net/hls/live/' . $get;
+
+// Set headers for fetching remote
+$mpdheads = [
+  'http' => [
+      'header' => "User-Agent:  Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.101 Mobile Safari/537.36\r\n",
+      'follow_location' => 1,
+      'timeout' => 5
+  ]
+];
+$context = stream_context_create($mpdheads);
+
+// Fetch and output
+$res = @file_get_contents($mpdUrl, false, $context);
+
+if ($res === false) {
+    http_response_code(502);
+    echo "Failed to fetch resource.";
+} else {
+    // Optionally forward content-type
+    header("Content-Type: application/dash+xml");
+    echo $res;
+}
+?>
